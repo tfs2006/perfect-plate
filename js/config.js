9fe8e0c8e6584c1bbd78d62c6733ba68d78e569e
TFS2006 <56497278+tfs2006@users.noreply.github.com>
2025-08-11 15:31:40 -0600

    Point API to Netlify functions (prod)

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 // If using Netlify for functions + GitHub Pages for the static site:
 //   set API_BASE to "https://YOUR-NETLIFY-SITE.netlify.app/.netlify/functions"
 // If deploying everything on Netlify, you can leave it empty and calls will hit same-origin.
-window.API_BASE = "http://localhost:8888/.netlify/functions";
+window.API_BASE = "https://perfect-plate-app.netlify.app/.netlify/functions";
